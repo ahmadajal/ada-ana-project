75869a55179d941286e38b34b2d156d29fab71bb
amirsh <amir.shaikhha@epfl.ch>
2016-12-07 17:57:41 +0100

    init commit for json parser

---
+++ json-parse/Main.scala
@@ -0,0 +1,33 @@
+package ch.epfl.ada
+
+import org.json4s._
+import org.json4s.native.JsonMethods._
+import java.io.FileReader
+import java.io.BufferedReader
+import java.io.FileWriter
+import java.io.BufferedWriter
+
+object Main {
+  def readPart(fileName: String, index: Int, size: Int): Array[Char] = {
+    val br = new BufferedReader(new FileReader(fileName))
+    val arr = new Array[Char](size)
+    br.read(arr, index, size)
+    arr
+  }
+  def writeBytes(fileName: String, arr: Array[Char]): Unit = {
+    val bw = new BufferedWriter(new FileWriter(fileName))
+    bw.write(arr)
+    bw.close()
+  }
+  def filterFields(js: JValue): JValue = {
+    val fields = List("lang", "sentiment")
+    val children = js.children
+    // val retrievedFields = fields.map(f => JObject(children.map(c => (js \ "_source").filterField(x => x._1 == "lang"))))
+    val retrievedFields = children.map(c => JObject((js \ "_source").filterField(x => fields.contains(x._1))))
+    JArray(retrievedFields)
+  }
+  def main(args: Array[String]) {
+    val res = parse(""" { "numbers" : [1, 2, 3, 4] } """)
+    println(res)
+  }
+}
---
+++ json-parse/build.sbt
@@ -0,0 +1,7 @@
+scalaVersion := "2.11.4"
+
+libraryDependencies += "org.json4s" %% "json4s-native" % "3.5.0"
+
+libraryDependencies += "org.json4s" %% "json4s-jackson" % "3.5.0"
+
+scalacOptions += "-language:implicitConversions"
